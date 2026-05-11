b0309bd41373a030374315c2d76fd592bdf96f3c
Juanfelipe0328 <jbustamante@estudiante.uniajc.edu.co>
2026-05-10 23:12:01 -0500

    Este es el ejemeplo correcto

---
+++ SRC/FacturaC.java
@@ -0,0 +1,22 @@
+package SRC;
+
+class FacturaC {
+
+    public void calcularTotal() {
+        // lógica para calcular total
+    }
+}
+
+class FacturaRepositorio {
+
+    public void guardarFactura() {
+        // lógica para guardar en base de datos
+    }
+}
+
+class EmailServicio {
+
+    public void enviarEmail() {
+        // lógica para enviar correo
+    }
+}
